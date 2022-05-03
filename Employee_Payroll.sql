4300fe2855f2dba30fa9051b3b5b3e7d5c11ef9a
unknown <mohit16gaikwad@gmail.com>
2022-05-03 20:11:18 +0530

    Added: Ability to create a employee payroll table to manage employee payroll

---
+++ Employee_Payroll.sql
@@ -0,0 +1,8 @@
+create table employee_payroll
+( id int not null auto_increment,
+name varchar(50) not null,
+salary Double not null,
+startDate date not null,
+primary key(id)
+);
+desc employee_payroll;
