576835c48e393474e718aac85caa08051f8cdca5
MOHIT BHASKARRAO GAIKWAD <97322025+gaikwadmohit@users.noreply.github.com>
2022-05-05 15:33:17 +0530

    Ability to create a employee payroll table in the payroll service database to manage employee payrolls

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -1,4 +1,15 @@
+#UC1 
 create database payroll_service;
 show databases;
 use payroll_service;
 select database();
+
+#UC2 
+create table employee_payroll
+( id int not null auto_increment,
+name varchar(50) not null,
+salary Double not null,
+startDate date not null,
+primary key(id)
+);
+desc employee_payroll;
