3f328ed070c2c05519e7d4195dc2d494e1790d50
unknown <mohit16gaikwad@gmail.com>
2022-05-05 13:57:23 +0530

    Added: Ability to find sum , max, min ,count of male and female

---
+++ sumAvgMinMaxCount.sql
@@ -0,0 +1,6 @@
+select gender,sum(salary) from employee_payroll group by gender; 
+select gender,min(salary) from employee_payroll;
+select gender,max(salary) from employee_payroll;
+select gender,avg(salary) from employee_payroll;
+select count(salary) from employee_payroll;
+set sql_safe_updates=0;
