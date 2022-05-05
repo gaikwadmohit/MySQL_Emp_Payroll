765879aa0499800cb4ec714686ffd9dc5bdfb6fc
MOHIT BHASKARRAO GAIKWAD <97322025+gaikwadmohit@users.noreply.github.com>
2022-05-05 15:36:52 +0530

    Ability to create employee payroll data in the payroll service database as part of CRUD Operation

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -13,3 +13,11 @@ startDate date not null,
 primary key(id)
 );
 desc employee_payroll;
+
+#UC3 
+select * from employee_payroll;
+insert into employee_payroll (name,salary,startDate) values('Rajendra',40000,'2021-10-11');
+insert into employee_payroll (name,salary,startDate) values(5,'Megha',70000,'2021-9-11');
+insert into employee_payroll (name,salary,startDate) values('Shushant',40000,'2021-1-17');
+insert into employee_payroll(name,salary,startdate)values('Nirmal',70000,'2022-05-10');
+insert into employee_payroll(name,salary,startdate)values('Ragul',60000,'2021-11-15');
