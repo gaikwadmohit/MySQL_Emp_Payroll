fd8e9aaf230e992735b5a53faf9502329481a09f
MOHIT BHASKARRAO GAIKWAD <97322025+gaikwadmohit@users.noreply.github.com>
2022-05-05 15:38:59 +0530

    Ability to retrieve all the employee payroll data that is added to payroll service database

--- Payroll_Service.sql
+++ Payroll_Service.sql
@@ -21,3 +21,6 @@ insert into employee_payroll (name,salary,startDate) values(5,'Megha',70000,'202
 insert into employee_payroll (name,salary,startDate) values('Shushant',40000,'2021-1-17');
 insert into employee_payroll(name,salary,startdate)values('Nirmal',70000,'2022-05-10');
 insert into employee_payroll(name,salary,startdate)values('Ragul',60000,'2021-11-15');
+
+#UC4 
+select * from employee_payroll;
