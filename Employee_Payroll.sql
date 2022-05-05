c40d41dbe39c62b6b8cc708a9d2674ee78aa8996
MOHIT BHASKARRAO GAIKWAD <97322025+gaikwadmohit@users.noreply.github.com>
2022-05-05 15:44:59 +0530

    Delete Employee_Payroll.sql

--- Employee_Payroll.sql
+++
@@ -1,8 +0,0 @@
-create table employee_payroll
-( id int not null auto_increment,
-name varchar(50) not null,
-salary Double not null,
-startDate date not null,
-primary key(id)
-);
-desc employee_payroll;
