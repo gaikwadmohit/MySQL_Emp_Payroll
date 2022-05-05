b8468e49fc5611bf461bf05c3336d4073c28b393
MOHIT BHASKARRAO GAIKWAD <97322025+gaikwadmohit@users.noreply.github.com>
2022-05-05 15:44:33 +0530

    Delete Add_Gender.sql

--- Add_Gender.sql
+++
@@ -1,4 +0,0 @@
-alter table employee_payroll add gender char(1);
-UPDATE employee_payroll set gender='F' where id='2';
-UPDATE employee_payroll set gender = 'M' WHERE id =1;
-UPDATE employee_payroll set gender = 'M' WHERE id =4;
